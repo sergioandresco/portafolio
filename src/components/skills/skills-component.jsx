19e1f812284eb38196be943500b139ba33222167
sergioandresco <sergiocobos6@outlook.com>
2023-12-06 23:00:20 -0500

    creation portfolio for desktop view

--- src/components/skills/skills-component.jsx
+++ src/components/skills/skills-component.jsx
@@ -20,11 +20,11 @@ const InterFont3 = Inter({
 export default function SkillsComponent(){
     return(
 
-        <div className="projects--container_description">
+        <div className="skills--container_description">
 
             <div className="projects--txt_1">
 
-                <div className="projects--sub_containr_txt_1">
+                <div className="skills--sub_containr_txt_1">
 
                     <div className="skills--container_skills">
 
@@ -36,7 +36,7 @@ export default function SkillsComponent(){
 
                             </div>
 
-                            <div className="skills--name_icon">
+                            <div className="skills--name_icon name">
 
                                 <span>PostgreSQL</span>
 
@@ -44,108 +44,295 @@ export default function SkillsComponent(){
 
                         </div>
 
-                        
+                        <div className="skills--container_father_icon">
 
-                        <div className="skills--container_icon">
+                            <div className="skills--container_icon">
 
-                            <img src="/images/icons/javascript.svg" alt="javascript" className='skills--icon' />
+                                <img src="/images/icons/javascript.svg" alt="javascript" className='skills--icon' />
 
-                        </div>
+                            </div>
 
-                        <div className="skills--container_icon">
+                            <div className="skills--name_icon name">
 
-                            <img src="/images/icons/css.svg" alt="css" className='skills--icon' />
+                                <span>JavaScript</span>
 
-                        </div>
-
-                        <div className="skills--container_icon">
-
-                            <img src="/images/icons/react.svg" alt="react" className='skills--icon' />
+                            </div>
 
                         </div>
 
-                        <div className="skills--container_icon">
+                        <div className="skills--container_father_icon">
 
-                            <img src="/images/icons/html5.svg" alt="html5" className='skills--icon' />
+                            <div className="skills--container_icon">
 
-                        </div>
+                                <img src="/images/icons/css.svg" alt="css" className='skills--icon' />
 
-                        <div className="skills--container_icon">
+                            </div>
 
-                            <img src="/images/icons/git.svg" alt="git" className='skills--icon' />
+                            <div className="skills--name_icon name">
 
-                        </div>
+                                <span>CSS</span>
 
-                        <div className="skills--container_icon">
-
-                            <img src="/images/icons/python.svg" alt="python" className='skills--icon' />
+                            </div>
 
                         </div>
 
-                        <div className="skills--container_icon">
+                        <div className="skills--container_father_icon">
 
-                            <img src="/images/icons/nextjs.svg" alt="nextjs" className='skills--icon' />
+                            <div className="skills--container_icon">
 
-                        </div>
+                                <img src="/images/icons/react.svg" alt="react" className='skills--icon' />
 
-                        <div className="skills--container_icon">
+                            </div>
 
-                            <img src="/images/icons/mysql.svg" alt="mysql" className='skills--icon' />
+                            <div className="skills--name_icon name">
 
-                        </div>
+                                <span>React</span>
 
-                        <div className="skills--container_icon">
-
-                            <img src="/images/icons/cpanel.svg" alt="cpanel" className='skills--icon' />
+                            </div>
 
                         </div>
 
-                        <div className="skills--container_icon">
+                        <div className="skills--container_father_icon">
 
-                            <img src="/images/icons/figma.svg" alt="figma" className='skills--icon' />
+                            <div className="skills--container_icon">
 
-                        </div>
+                                <img src="/images/icons/html5.svg" alt="html5" className='skills--icon' />
 
-                        <div className="skills--container_icon">
+                            </div>
 
-                            <img src="/images/icons/sass.svg" alt="sass" className='skills--icon' />
+                            <div className="skills--name_icon name">
 
-                        </div>
+                                <span>HTML5</span>
 
-                        <div className="skills--container_icon">
-
-                            <img src="/images/icons/ubuntu.svg" alt="ubuntu" className='skills--icon' />
+                            </div>
 
                         </div>
 
-                        <div className="skills--container_icon">
+                        <div className="skills--container_father_icon">
 
-                            <img src="/images/icons/amazon_aws.svg" alt="AWS" className='skills--icon' />
+                            <div className="skills--container_icon">
 
-                        </div>
+                                <img src="/images/icons/git.svg" alt="git" className='skills--icon' />
 
-                        <div className="skills--container_icon">
+                            </div>
 
-                            <img src="/images/icons/php.svg" alt="php" className='skills--icon' />
+                            <div className="skills--name_icon name">
 
-                        </div>
+                                <span>Git</span>
 
-                        <div className="skills--container_icon">
-
-                            <img src="/images/icons/postman.svg" alt="postman" className='skills--icon' />
+                            </div>
 
                         </div>
 
-                        <div className="skills--container_icon">
+                        <div className="skills--container_father_icon">
 
-                            <img src="/images/icons/bootstrap.svg" alt="bootstrap" className='skills--icon' />
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/python.svg" alt="python" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Python</span>
+
+                            </div>
 
                         </div>
 
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/nextjs.svg" alt="nextjs" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>NextJS</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/mysql.svg" alt="mysql" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>MySQL</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/cpanel.svg" alt="cpanel" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>CPanel</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/figma.svg" alt="figma" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Figma</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/sass.svg" alt="sass" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Sass</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/ubuntu.svg" alt="ubuntu" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Ubuntu</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/amazon_aws.svg" alt="AWS" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>AWS</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/php.svg" alt="php" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>PHP</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/postman.svg" alt="postman" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Postman</span>
+
+                            </div>
+
+                        </div>
+
+                        <div className="skills--container_father_icon">
+
+                            <div className="skills--container_icon">
+
+                                <img src="/images/icons/bootstrap.svg" alt="bootstrap" className='skills--icon' />
+
+                            </div>
+
+                            <div className="skills--name_icon name">
+
+                                <span>Bootstrap</span>
+
+                            </div>
+
+                        </div>
 
                     </div>
 
                     <div className="skills--container_footer_skills">
+
+                        <div className="skills--subcontainer_footer_skills">
+
+                            <div className="skills--footer_txt">
+
+                                <span className={`${InterFont3.className} skills--from`}>from <strong className={`${InterFont2.className}`}>Colombia</strong></span>
+
+                            </div>
+
+                            <a href="https://github.com/sergioandresco">
+
+                                <div className="skills--footer_img">
+
+                                    <img src="/images/icons/github.svg" alt="github" className='skills--icon_footer' />
+
+                                </div>
+
+                            </a>
+
+                            <a href="https://www.linkedin.com/in/sergio-andres-cobos-suarez-942637219/">
+
+                                <div className="skills--footer_img">
+
+                                    <img src="/images/icons/linkedin.svg" alt="linkedin" className='skills--icon_footer' />
+
+                                </div>
+
+                            </a>
+
+                        </div>
 
                     </div>
                     
