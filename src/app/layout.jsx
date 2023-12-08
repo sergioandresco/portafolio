55e4c657d3bd3fbb88ce93687e72ccfb3edd245e
sergioandresco <sergiocobos6@outlook.com>
2023-12-08 15:11:15 -0500

    add the head label

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,12 @@
+import Head from 'next/head';
 
 export default function RootLayout({ children }){
     return(
         <html lang="en">
+
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
 
             <body>
 
